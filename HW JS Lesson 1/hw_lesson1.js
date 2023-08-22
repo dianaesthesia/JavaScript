223b8a2a71503542fe7ae9102bfa37721875d48b
dianaesthesia <bereziukdiana@gmail.com>
2023-08-22 17:19:59 +0300

    HW Lesson 1

---
+++ HW JS Lesson 1/hw_lesson1.js
@@ -0,0 +1,48 @@
+/*-----------------------------------------Task1-------------------------------------------------------*/
+let s1 = 'hello';
+let s2 = 'owu';
+let s3 = 'com';
+let s4 = 'ua';
+console.log(s1, s2, s3, s4);
+
+let n1 = 1;
+let n2 = 10;
+let n3 = -999;
+let n4 = 123;
+let pi = 3.14;
+let n5 = 2.7;
+let n6= 16;
+console.log(n1, n2, n3, n4, pi, n5, n6);
+
+let b1 = true;
+console.log(b1);
+
+let b2 = false;
+console.log(b2);
+
+/*-----------------------------------------Task2-------------------------------------------------------*/
+let firstName = 'Березюк';
+let middleName = 'Діана';
+let lastName = 'Богданівна';
+let person = `${firstName} ${middleName} ${lastName}`;
+console.log(person);
+
+/*-----------------------------------------Task3-------------------------------------------------------*/
+let a = 100;
+console.log(typeof a, a);
+
+let b = '100';
+console.log(typeof b, b);
+
+let c = true;
+console.log(typeof c, c);
+
+/*-----------------------------------------Task4-------------------------------------------------------*/
+let nameUser = prompt('Enter your name:');
+console.log(nameUser);
+
+let surnameUser = prompt('Enter your surname:');
+console.log(surnameUser);
+
+let age = prompt('Enter your age:');
+console.log(age);
